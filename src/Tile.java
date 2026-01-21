d28d53c8980e3a78106dc860274420852a2bf666
azuru <saloarttu@outlook.com>
2026-01-21 16:01:02 +0200

    Add methods to Tile interface

--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,9 @@
 public interface Tile {
+    private void getCharacter() {
+        // TODO: return character that represents the tile type
+    }
+
+    private String getType() {
+        // TODO: return the tile type as string
+    }
 }
